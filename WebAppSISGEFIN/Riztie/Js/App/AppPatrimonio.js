228d69c5b867b3847c4b92b91577193ff49c5dfe
jhonn <jhonn2992@gmail.com>
2022-09-08 09:01:07 -0500

    crud Patrimonio marca y causal baja

--- WebAppSISGEFIN/Riztie/Js/App/AppPatrimonio.js
+++ WebAppSISGEFIN/Riztie/Js/App/AppPatrimonio.js
@@ -39,6 +39,39 @@ function configurarBotones() {
         limpiarForm("Popup");
     }
 
+    var btnGuardar = document.getElementById("btnGuardar");
+    if (btnGuardar != null) btnGuardar.onclick = function () {
+        var validar = false;
+
+        if (validarInformacion("Reque") == true) {
+            validar = true;
+        }
+        if (validar == true) {
+            Swal.fire({
+                title: '¿Desea grabar la información?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si',
+                cancelButtonText: 'No'
+            }).then((result) => {
+                if (result.value) {
+                    grabarDatos();
+
+                    Swal.fire({
+                        title: 'Procesando...',
+                        allowEscapeKey: false,
+                        allowOutsideClick: false,
+                        onOpen: () => {
+                            Swal.showLoading()
+                        }
+                    })
+                }
+            })
+        }
+    }
+
     var btnCancelar = document.getElementById("btnCancelar");
     if (btnCancelar != null) btnCancelar.onclick = function () {
         divPopupContainer.style.display = 'none';
@@ -126,10 +159,162 @@ function mostrarRegistro(rpta) {
 
 }
 
+function eliminarRegistro(id) {
+    var data = "";
+    data = id;
+
+    var frm = new FormData();
+    frm.append("data", data);
+
+    Swal.fire({
+        title: '¿Desea eliminar el registro?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No'
+    }).then((result) => {
+        if (result.value) {
+            Http.post("General/eliminar/?tbl=" + controller + vista, mostrarEliminar, frm);
+        }
+    })
+}
+
+function mostrarEliminar(rpta) {
+    var mensajeResul = [];
+    if (rpta) {
+        listas = rpta.split("¯")
+        lista = listas[0].split("¬");
+        mensajeResul = listas[1].split("|");
+        var tipo = mensajeResul[0];
+        var mensaje = mensajeResul[1];
+        grillaItem = new GrillaScroll(lista, "divLista", 100, 6, vista, controller, null, null, null, botones, 38, false, null);
+
+        var cbo = document.getElementById("cboPadre");
+        if (cbo != null) {
+            var listaPadre = listas[2].split("¬");
+            crearCombo(listaPadre, "cboPadre", "Ninguno");
+        }
+
+        if (tipo == 'A') {
+            Swal.fire({
+                title: 'Eliminado!',
+                text: mensaje,
+                icon: 'success',
+                showConfirmButton: true,
+                timer: 2000
+            })
+        }
+        else {
+            Swal.fire({
+                title: 'Advertencia!',
+                text: mensaje,
+                icon: 'warning',
+                showConfirmButton: true,
+                timer: 2000
+            })
+        }
+    }
+    else {
+        mostrarMensaje("No se proceso el registro - verifique por favor", "error");
+    }
+}
+
 function seleccionarFila(fila, id, prefijo) {
     idRegistro = id;
     window["id" + prefijo] = id;
     if (window["fila" + prefijo] != null) window["fila" + prefijo].className = "FilaDatos";
     fila.className = "FilaSeleccionada";
     window["fila" + prefijo] = fila;
+}
+
+function grabarDatos() {
+    var data = ""
+    var frm = new FormData();
+    data = obtenerDatosGrabar("Popup");
+    frm.append("data", data);
+    console.log(data);
+    Http.post("General/guardar/?tbl=" + controller + vista, mostrarGrabar, frm);
+}
+
+function obtenerDatosGrabar(clase) {
+    var data = "";
+    var controles = document.getElementsByClassName(clase);
+    var control;
+    var nControles = controles.length;
+    for (var j = 0; j < nControles; j++) {
+        control = controles[j];
+        switch (control.tagName) {
+            case "INPUT":
+                if (control.id.substr(0, 3) == "txt") data += control.value;
+                if (control.id.substr(0, 3) == "num") data += control.value;
+                if (control.id.substr(0, 3) == "num") data += control.value;
+                if (control.id.substr(0, 3) == "dtt") {
+                    if (control.value != "") {
+                        var dFecha = control.value.split("-");
+                        data += dFecha[2] + "-" + dFecha[1] + "-" + dFecha[0];
+                    }
+                    else { data += ""; }
+                }
+                if (control.id.substr(0, 3) == "tim") data += control.value;
+                if (control.id.substr(0, 3) == "chk") data += (control.checked ? "1" : "0");
+                if (control.id.substr(0, 3) == "opt") data += (control.checked ? "1" : "0");
+                if (control.id.substr(0, 3) == "dtg") data += (control.checked ? "1" : "0");
+                break;
+            case "SELECT":
+                data += control.value;
+                break;
+            case "TEXTAREA":
+                data += control.value;
+                break;
+            case "IMG":
+                data += control.src.replace("data:image/jpeg;base64,", "");
+                break;
+        }
+        data += "|";
+    }
+    data = data.substr(0, data.length - 1);
+
+    return data;
+}
+
+function mostrarGrabar(rpta) {
+    var mensajeResul = [];
+    if (rpta) {
+        listas = rpta.split("¯")
+        lista = listas[0].split("¬");
+        mensajeResul = listas[1].split("|");
+        var tipo = mensajeResul[0];
+        var mensaje = mensajeResul[1];
+        divPopupContainer.style.display = 'none';
+        grillaItem = new GrillaScroll(lista, "divLista", 100, 6, vista, controller, null, null, true, botones, 38, false, null);
+
+
+        if (tipo == 'A') {
+            Swal.fire({
+                title: 'Finalizado!',
+                text: mensaje,
+                icon: 'success',
+                showConfirmButton: true,
+                timer: 2000
+            })
+            alerta = 'success';
+        }
+        else {
+            Swal.fire({
+                title: 'Error!',
+                text: mensaje,
+                icon: 'error',
+                showConfirmButton: true,
+                timer: 2000
+            })
+        }
+    }
+    else {
+        mostrarMensaje("No se realizó el registro", "error")
+    }
+
+    btnGuardar.innerHTML = "<i class='fa fa-save'></i> Grabar";
+    btnGuardar.disabled = false;
 }
