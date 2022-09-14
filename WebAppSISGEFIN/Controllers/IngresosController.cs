4ccabaacd6049ab9943201c1cc384691f81d738a
vfg2022 <vid_2022dal@hotmail.com>
2022-09-14 09:29:30 -0500

    Asegurado

--- WebAppSISGEFIN/Controllers/IngresosController.cs
+++ WebAppSISGEFIN/Controllers/IngresosController.cs
@@ -19,6 +19,7 @@ namespace WebAppSISGEFIN.Controllers
         [FiltroAutenticacion]
         public ActionResult Asegurado()
         {
+
             string[] Data = Session["DataUsuario"].ToString().Split('|');
             string IdPerfil = Data[3];
             ViewBag.Menu = new Menu().Listar(IdPerfil);
